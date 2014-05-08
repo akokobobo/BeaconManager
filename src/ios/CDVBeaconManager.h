c9b1cad3291ac2ad9045dbb8226994897d71bac0
Adrian Kokobobo <akokobobo@gmail.com>
2014-05-08 13:14:29 -0400

    -

--- src/ios/CDVBeaconManager.h
+++ src/ios/CDVBeaconManager.h
@@ -5,6 +5,6 @@
 @interface CDVBeaconManager : CDVPlugin
 
 - (void) startMonitoring:(CDVInvokedUrlCommand*)command;
-- (void) currentBeacon:(CDVInvokedUrlCommand*)command
+- (void) currentBeacon:(CDVInvokedUrlCommand*)command;
 
 @end
